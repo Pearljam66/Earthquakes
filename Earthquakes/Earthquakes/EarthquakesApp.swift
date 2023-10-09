f29669962d0882497b9a52147aa742db326ef020
Sarah Clark <sarahireneclark@gmail.com>
2023-10-09 13:34:40 -0500

    Initial commit of project, including app icon

---
+++ Earthquakes/Earthquakes/EarthquakesApp.swift
@@ -0,0 +1,17 @@
+//
+//  EarthquakesApp.swift
+//  Earthquakes
+//
+//  Created by Sarah Clark on 10/9/23.
+//
+
+import SwiftUI
+
+@main
+struct EarthquakesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Earthquakes/Earthquakes/Models/Quake.swift
@@ -0,0 +1,8 @@
+//
+//  Quake.swift
+//  Earthquakes
+//
+//  Created by Sarah Clark on 10/9/23.
+//
+
+import Foundation
